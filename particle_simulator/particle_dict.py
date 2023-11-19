d47631b33115bee95c6689bc77abe8a6faf2cdd7
rdesparbes <desparbes.romain@hotmail.fr>
2023-11-19 17:30:27 +0100

    Added a ParticleDict class to describe more precisely the dicts that are transmitted between the GUI and the Simulation

---
+++ particle_simulator/particle_dict.py
@@ -0,0 +1,21 @@
+from typing import TypedDict, Tuple, Literal, Union, NotRequired
+
+
+class ParticleDict(TypedDict):
+    radius: NotRequired[int]
+    color: NotRequired[Union[Tuple[int, int, int], Literal["random"]]]
+    mass: NotRequired[float]
+    velocity: NotRequired[Tuple[float, float]]
+    bounciness: NotRequired[float]
+    locked: NotRequired[bool]
+    collisions: NotRequired[bool]
+    attract_r: NotRequired[int]
+    repel_r: NotRequired[int]
+    attraction_strength: NotRequired[float]
+    repulsion_strength: NotRequired[float]
+    linked_group_particles: NotRequired[bool]
+    link_attr_breaking_force: NotRequired[float]
+    link_repel_breaking_force: NotRequired[float]
+    group: NotRequired[str]
+    separate_group: NotRequired[bool]
+    gravity_mode: NotRequired[bool]
