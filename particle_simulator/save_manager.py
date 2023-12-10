19f7a54635184a50087a945698524ec321a32d3d
rdesparbes <desparbes.romain@hotmail.fr>
2023-12-10 22:34:06 +0100

    Added optional filename parameters to the load and save methods of the SaveManager

--- particle_simulator/save_manager.py
+++ particle_simulator/save_manager.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import tkinter as tk
 from tkinter.filedialog import asksaveasfilename, askopenfilename
+from typing import Optional
 
 from .error import Error
 from .particle import Particle
@@ -13,109 +14,111 @@ class SaveManager:
         self.file_location = os.path.dirname(self.sim.gui.path)
         self.filename = "simulation"
 
-    def save(self):
-        filename = asksaveasfilename(
-            initialdir=self.file_location,
-            initialfile=self.filename,
-            defaultextension=".sim",
-            filetypes=[("Simulation files", "*.sim"), ("All Files", "*.*")],
-        )
+    def save(self, filename: Optional[str] = None) -> None:
+        if filename is None:
+            filename = asksaveasfilename(
+                initialdir=self.file_location,
+                initialfile=self.filename,
+                defaultextension=".sim",
+                filetypes=[("Simulation files", "*.sim"), ("All Files", "*.*")],
+            )
 
-        if filename != "":
-            try:
-                sim_settings = {
-                    "gravity_entry": [self.sim.gui.gravity_entry.get(), "entry"],
-                    "air_res_entry": [self.sim.gui.air_res_entry.get(), "entry"],
-                    "friction_entry": [self.sim.gui.friction_entry.get(), "entry"],
-                    "temp_sc": [self.sim.gui.temp_sc.get(), "set"],
-                    "speed_sc": [self.sim.gui.speed_sc.get(), "set"],
-                    "show_fps": [self.sim.gui.show_fps.get(), "set"],
-                    "show_num": [self.sim.gui.show_num.get(), "set"],
-                    "show_links": [self.sim.gui.show_links.get(), "set"],
-                    "top_bool": [self.sim.gui.top_bool.get(), "set"],
-                    "bottom_bool": [self.sim.gui.bottom_bool.get(), "set"],
-                    "left_bool": [self.sim.gui.left_bool.get(), "set"],
-                    "right_bool": [self.sim.gui.right_bool.get(), "set"],
-                    "grid_bool": [self.sim.gui.grid_bool.get(), "set"],
-                    "grid_res_x_value": [self.sim.gui.grid_res_x_value.get(), "set"],
-                    "grid_res_y_value": [self.sim.gui.grid_res_y_value.get(), "set"],
-                    "delay_entry": [self.sim.gui.delay_entry.get(), "entry"],
-                    "calculate_radii_diff_bool": [
-                        self.sim.gui.calculate_radii_diff_bool.get(),
-                        "set",
-                    ],
-                    "g_dir": [self.sim.g_dir, "var"],
-                    "wind_force": [self.sim.wind_force, "var"],
-                    "stress_visualization": [self.sim.stress_visualization, "var"],
-                    "bg_color": [self.sim.bg_color, "var"],
-                    "void_edges": [self.sim.void_edges, "var"],
-                    "code": [self.sim.code, "var"],
-                }
+            if not filename:
+                return
+        try:
+            sim_settings = {
+                "gravity_entry": [self.sim.gui.gravity_entry.get(), "entry"],
+                "air_res_entry": [self.sim.gui.air_res_entry.get(), "entry"],
+                "friction_entry": [self.sim.gui.friction_entry.get(), "entry"],
+                "temp_sc": [self.sim.gui.temp_sc.get(), "set"],
+                "speed_sc": [self.sim.gui.speed_sc.get(), "set"],
+                "show_fps": [self.sim.gui.show_fps.get(), "set"],
+                "show_num": [self.sim.gui.show_num.get(), "set"],
+                "show_links": [self.sim.gui.show_links.get(), "set"],
+                "top_bool": [self.sim.gui.top_bool.get(), "set"],
+                "bottom_bool": [self.sim.gui.bottom_bool.get(), "set"],
+                "left_bool": [self.sim.gui.left_bool.get(), "set"],
+                "right_bool": [self.sim.gui.right_bool.get(), "set"],
+                "grid_bool": [self.sim.gui.grid_bool.get(), "set"],
+                "grid_res_x_value": [self.sim.gui.grid_res_x_value.get(), "set"],
+                "grid_res_y_value": [self.sim.gui.grid_res_y_value.get(), "set"],
+                "delay_entry": [self.sim.gui.delay_entry.get(), "entry"],
+                "calculate_radii_diff_bool": [
+                    self.sim.gui.calculate_radii_diff_bool.get(),
+                    "set",
+                ],
+                "g_dir": [self.sim.g_dir, "var"],
+                "wind_force": [self.sim.wind_force, "var"],
+                "stress_visualization": [self.sim.stress_visualization, "var"],
+                "bg_color": [self.sim.bg_color, "var"],
+                "void_edges": [self.sim.void_edges, "var"],
+                "code": [self.sim.code, "var"],
+            }
 
-                particle_settings = {
-                    "radius_entry": [self.sim.gui.radius_entry.get(), "entry"],
-                    "color_entry": [self.sim.gui.color_entry.get(), "entry"],
-                    "mass_entry": [self.sim.gui.mass_entry.get(), "entry"],
-                    "velocity_x_entry": [self.sim.gui.velocity_x_entry.get(), "entry"],
-                    "velocity_y_entry": [self.sim.gui.velocity_y_entry.get(), "entry"],
-                    "bounciness_entry": [self.sim.gui.bounciness_entry.get(), "entry"],
-                    "do_collision_bool": [self.sim.gui.do_collision_bool.get(), "set"],
-                    "locked_bool": [self.sim.gui.locked_bool.get(), "set"],
-                    "linked_group_bool": [self.sim.gui.linked_group_bool.get(), "set"],
-                    "attr_r_entry": [self.sim.gui.attr_r_entry.get(), "entry"],
-                    "repel_r_entry": [self.sim.gui.repel_r_entry.get(), "entry"],
-                    "attr_strength_entry": [
-                        self.sim.gui.attr_strength_entry.get(),
-                        "entry",
-                    ],
-                    "gravity_mode_bool": [self.sim.gui.gravity_mode_bool.get(), "set"],
-                    "repel_strength_entry": [
-                        self.sim.gui.repel_strength_entry.get(),
-                        "entry",
-                    ],
-                    "link_attr_break_entry": [
-                        self.sim.gui.link_attr_break_entry.get(),
-                        "entry",
-                    ],
-                    "link_repel_break_entry": [
-                        self.sim.gui.link_repel_break_entry.get(),
-                        "entry",
-                    ],
-                    "groups_entry": [self.sim.gui.groups_entry.get(), "entry"],
-                    "separate_group_bool": [
-                        self.sim.gui.separate_group_bool.get(),
-                        "set",
-                    ],
-                }
+            particle_settings = {
+                "radius_entry": [self.sim.gui.radius_entry.get(), "entry"],
+                "color_entry": [self.sim.gui.color_entry.get(), "entry"],
+                "mass_entry": [self.sim.gui.mass_entry.get(), "entry"],
+                "velocity_x_entry": [self.sim.gui.velocity_x_entry.get(), "entry"],
+                "velocity_y_entry": [self.sim.gui.velocity_y_entry.get(), "entry"],
+                "bounciness_entry": [self.sim.gui.bounciness_entry.get(), "entry"],
+                "do_collision_bool": [self.sim.gui.do_collision_bool.get(), "set"],
+                "locked_bool": [self.sim.gui.locked_bool.get(), "set"],
+                "linked_group_bool": [self.sim.gui.linked_group_bool.get(), "set"],
+                "attr_r_entry": [self.sim.gui.attr_r_entry.get(), "entry"],
+                "repel_r_entry": [self.sim.gui.repel_r_entry.get(), "entry"],
+                "attr_strength_entry": [
+                    self.sim.gui.attr_strength_entry.get(),
+                    "entry",
+                ],
+                "gravity_mode_bool": [self.sim.gui.gravity_mode_bool.get(), "set"],
+                "repel_strength_entry": [
+                    self.sim.gui.repel_strength_entry.get(),
+                    "entry",
+                ],
+                "link_attr_break_entry": [
+                    self.sim.gui.link_attr_break_entry.get(),
+                    "entry",
+                ],
+                "link_repel_break_entry": [
+                    self.sim.gui.link_repel_break_entry.get(),
+                    "entry",
+                ],
+                "groups_entry": [self.sim.gui.groups_entry.get(), "entry"],
+                "separate_group_bool": [
+                    self.sim.gui.separate_group_bool.get(),
+                    "set",
+                ],
+            }
 
-                data = {
-                    "particles": [
-                        particle.return_dict(index_source=self.sim.particles)
-                        for particle in self.sim.particles
-                    ],
-                    "particle-settings": particle_settings,
-                    "sim-settings": sim_settings,
-                }
-                with open(filename, "wb") as file_object:
-                    pickle.dump(data, file_object)
+            data = {
+                "particles": [
+                    particle.return_dict(index_source=self.sim.particles)
+                    for particle in self.sim.particles
+                ],
+                "particle-settings": particle_settings,
+                "sim-settings": sim_settings,
+            }
+            with open(filename, "wb") as file_object:
+                pickle.dump(data, file_object)
 
-                self.file_location, self.filename = os.path.split(filename)
-            except Exception as error:
-                self.sim.error = Error("Saving-Error", error)
+            self.file_location, self.filename = os.path.split(filename)
+        except Exception as error:
+            self.sim.error = Error("Saving-Error", error)
 
-    def load(self):
+    def load(self, filename: Optional[str] = None):
         if not self.sim.paused:
             self.sim.toggle_paused()
+        if filename is None:
+            filename = askopenfilename(
+                initialdir=self.file_location,
+                initialfile=self.filename,
+                defaultextension=".sim",
+                filetypes=[("Simulation files", "*.sim"), ("All Files", "*.*")],
+            )
 
-        filename = askopenfilename(
-            initialdir=self.file_location,
-            initialfile=self.filename,
-            defaultextension=".sim",
-            filetypes=[("Simulation files", "*.sim"), ("All Files", "*.*")],
-        )
-
-        if filename == "":
-            return
+            if not filename:
+                return
         try:
             with open(filename, "rb") as file_object:
                 data = pickle.load(file_object)
