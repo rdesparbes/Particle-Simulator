rdesparbes/Particle-Simulator

commit 3b6fbd7b6a9c5b6d1ac9b0915a50d2eff01a429b

Removed is_in_group and is_linked parameters in some methods

particle_simulator/particle.py
@@ -48,17 +48,16 @@ class Particle(ParticleData):
         repel_r: Optional[float],
         attr: float,
         repel: float,
-        is_in_group: bool,
-        is_linked: bool,
         gravity: bool,
     ) -> float:
         repel_r = part.repel_r if repel_r is None else repel_r
+        is_linked = self._is_linked_to(part)
         magnitude = self._compute_magnitude(
             part,
             attr,
             distance,
             gravity,
-            is_in_group,
+            self._is_in_same_group(part),
             is_linked,
             repel,
             repel_r,
@@ -188,14 +187,20 @@ class Particle(ParticleData):
 
         self._collisions = {}
 
+    def _is_linked_to(self, p: Self) -> bool:
+        return p in self.link_lengths
+
+    def _is_in_same_group(self, p: Self) -> bool:
+        return not self.separate_group and p.group == self.group
+
     def _compute_interactions(self, p: Self) -> None:
         if p == self:
             return
-        is_in_group = not self.separate_group and p in self._sim.groups[self.group]
-        is_linked = p in self.link_lengths
 
         if (
-            not self.linked_group_particles and not is_linked and is_in_group
+            not self.linked_group_particles
+            and not self._is_linked_to(p)
+            and self._is_in_same_group(p)
         ) or p in self._collisions:
             return
 
@@ -210,9 +215,7 @@ class Particle(ParticleData):
             self._interacts(distance),
         )
         if any(conditions):
-            force = self._compute_force(
-                p, direction, distance, conditions, is_linked, is_in_group
-            )
+            force = self._compute_force(p, direction, distance, conditions)
 
             self._apply_force(force)
             p._collisions[self] = -force
@@ -239,8 +242,6 @@ class Particle(ParticleData):
         direction: npt.NDArray[np.float_],
         distance: float,
         conditions: Tuple[bool, bool],
-        is_linked: bool,
-        is_in_group: bool,
     ) -> npt.NDArray[np.float_]:
         if distance == 0.0:
             if self.gravity_mode or p.gravity_mode:
@@ -251,8 +252,6 @@ class Particle(ParticleData):
             p,
             conditions,
             distance,
-            is_in_group,
-            is_linked,
         )
 
     def _calculate_magnitude(
@@ -260,8 +259,6 @@ class Particle(ParticleData):
         p: Self,
         distance: float,
         repel_r: Optional[float],
-        is_in_group: bool,
-        is_linked: bool,
     ) -> float:
         return self._calc_magnitude(
             part=p,
@@ -269,8 +266,6 @@ class Particle(ParticleData):
             repel_r=p.repel_r if repel_r is None else repel_r,
             attr=p.attr,
             repel=p.repel,
-            is_in_group=is_in_group,
-            is_linked=is_linked,
             gravity=p.gravity_mode,
         )
 
@@ -279,11 +274,9 @@ class Particle(ParticleData):
         p: Self,
         conditions: Tuple[bool, bool],
         distance: float,
-        is_in_group: bool,
-        is_linked: bool,
     ) -> float:
         repel_r: Optional[float] = None
-        if is_linked:
+        if self._is_linked_to(p):
             repel_radius = self.link_lengths[p]
             if repel_radius != "repel":
                 repel_r = repel_radius
@@ -295,8 +288,6 @@ class Particle(ParticleData):
                     p=p,
                     distance=distance,
                     repel_r=repel_r,
-                    is_in_group=is_in_group,
-                    is_linked=is_linked,
                 )
             magnitude = 0.0
             particles: List[Tuple[Particle, Particle]] = [(self, p), (p, self)]
@@ -306,8 +297,6 @@ class Particle(ParticleData):
                         p=particle_b,
                         distance=distance,
                         repel_r=repel_r,
-                        is_in_group=is_in_group,
-                        is_linked=is_linked,
                     )
             return magnitude
         repel_r_ = max(self.repel_r, p.repel_r) if repel_r is None else repel_r
@@ -317,8 +306,6 @@ class Particle(ParticleData):
             repel_r=repel_r_,
             attr=p.attr + self.attr,
             repel=p.repel + self.repel,
-            is_in_group=is_in_group,
-            is_linked=is_linked,
             gravity=self.gravity_mode or p.gravity_mode,
         )
         return magnitude

commit b3a8cf470ada38ccefd88e956ea805fa1608b5ab

Fixed bug visible in "soft_body.sim" introduced in 3b6fbd7b6a9c5b6d1ac9b0915a50d2eff01a429b

particle_simulator/particle.py
@@ -269,6 +269,7 @@ def default_compute_magnitude_strategy(
         repel_r=repel_r,
         attr=part_b.attraction_strength + part_a.attraction_strength,
         repel=part_b.repulsion_strength + part_a.repulsion_strength,
+        is_in_group=part_a._is_in_same_group(part_b),
         gravity=part_a.gravity_mode or part_b.gravity_mode,
     )
     return magnitude
@@ -281,6 +282,7 @@ def radii_compute_magnitude_strategy(
     repel_r: Optional[float],
 ) -> float:
     magnitude = 0.0
+    is_in_group = part_a._is_in_same_group(part_b)
     if part_b.reaches(distance):
         magnitude += part_a.calculate_magnitude(
             part=part_b,
@@ -288,6 +290,7 @@ def radii_compute_magnitude_strategy(
             repel_r=part_b.repel_r if repel_r is None else repel_r,
             attr=part_b.attraction_strength,
             repel=part_b.repulsion_strength,
+            is_in_group=is_in_group,
             gravity=part_b.gravity_mode,
         )
     if part_a.reaches(distance):
@@ -297,6 +300,7 @@ def radii_compute_magnitude_strategy(
             repel_r=part_a.repel_r if repel_r is None else repel_r,
             attr=part_a.attraction_strength,
             repel=part_a.repulsion_strength,
+            is_in_group=is_in_group,
             gravity=part_a.gravity_mode,
         )
     return magnitude

particle_simulator/particle_data.py
@@ -93,12 +93,13 @@ class ParticleData:
         distance: float,
         gravity: bool,
         repel: float,
+        is_in_group: bool,
         repel_r: float,
     ) -> float:
         rest_distance = abs(distance - repel_r)
         if distance < repel_r:
             return -repel * rest_distance / 10.0
-        if self._is_in_same_group(part) or self._is_linked_to(part):
+        if is_in_group or self._is_linked_to(part):
             if gravity:
                 return 10.0 * attr * self.mass * part.mass / distance**2
             return attr * rest_distance / 3000.0