e477d6bc970bdd7d560c280b687b11d0aaa5b0b1
rdesparbes <desparbes.romain@hotmail.fr>
2024-01-28 23:03:52 +0100

    Added tests for GUI.copy_selected_btn

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -2,10 +2,10 @@ from typing import List, Dict
 
 import pytest
 
-from particle_simulator.gui.gui import GUI
 from particle_simulator.engine.particle import Particle
 from particle_simulator.engine.particle_properties import ParticleProperties
 from particle_simulator.engine.simulation_state import SimulationState
+from particle_simulator.gui.gui import GUI
 
 
 def test_gui_can_display_sorted_groups_of_sim_state() -> None:
@@ -39,3 +39,64 @@ def test_gui_displays_expect_group_name(
     sim_state = SimulationState(groups=groups)
     gui = GUI(sim_state)
     assert gui.groups_entry.get() == expected_text
+
+
+@pytest.mark.parametrize("collisions", [True, False])
+def test_copy_selected_btn_invoke_without_selected_particles_changes_nothing(
+    collisions: bool,
+) -> None:
+    # Arrange
+    sim_state = SimulationState()
+    gui = GUI(sim_state)
+    gui.do_collision_bool.set(collisions)
+    # Act
+    gui.copy_selected_btn.invoke()
+    # Assert
+    assert gui.do_collision_bool.get() == collisions
+
+
+@pytest.mark.parametrize("collisions", [True, False])
+def test_copy_selected_btn_invoke_with_selected_particles_all_same_param_sets_param(
+    collisions: bool,
+) -> None:
+    """This test verifies that a value is set if all the selected particles have the same
+    value for the considered parameter
+    """
+    # Arrange
+    sim_state = SimulationState()
+    particles = [
+        Particle(0.0, 0.0, props=ParticleProperties(collisions=collisions)),
+        Particle(0.0, 0.0, props=ParticleProperties(collisions=collisions)),
+    ]
+    for particle in particles:
+        sim_state.register_particle(particle)
+        sim_state.select_particle(particle)
+    gui = GUI(sim_state)
+    # Act
+    gui.copy_selected_btn.invoke()
+    # Assert
+    assert gui.do_collision_bool.get() == collisions
+
+
+@pytest.mark.parametrize("collisions", [True, False])
+def test_copy_selected_btn_invoke_with_selected_particles_heterogeneous_param_does_not_sets_param(
+    collisions: bool,
+) -> None:
+    """This test verifies that a value is not set if at least one of the
+    selected particles have a different value from the others for the
+    considered parameter
+    """
+    # Arrange
+    sim_state = SimulationState()
+    particles = [
+        Particle(0.0, 0.0, props=ParticleProperties(collisions=collisions)),
+        Particle(0.0, 0.0, props=ParticleProperties(collisions=not collisions)),
+    ]
+    for particle in particles:
+        sim_state.register_particle(particle)
+        sim_state.select_particle(particle)
+    gui = GUI(sim_state)
+    # Act
+    gui.copy_selected_btn.invoke()
+    # Assert
+    assert not gui.do_collision_bool.get()
